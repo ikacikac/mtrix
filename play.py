f781fb6c9bed63d42b317b85f9865e87690138b5
Ilija Lazarevic <ilija.z.lazarevic@gmail.com>
2017-12-13 17:00:58 +0100

    Position windows centered on user screen

--- play.py
+++ play.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf8 -*-
 
+import os
 import pygame
 
 from internal_config import TITLE, ICON
 from game_states import *
 
 if __name__ == '__main__':
+
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
 
     pygame.init()
     pygame.font.init()
